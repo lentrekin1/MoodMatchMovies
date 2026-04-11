2fb7635aeda004285b824d4098c7f2b8f29d7935
M-a-r-l-K-a-r-x <63819889+M-a-r-l-K-a-r-x@users.noreply.github.com>
2026-04-11 14:19:42 -0400

    Backend database and posters

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ import json
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from models import db, Film
+import zipfile
 
 load_dotenv()
 from flask_cors import CORS
@@ -18,46 +20,75 @@ app = Flask(__name__,
 CORS(app)
 
 # Configure SQLite database - using 3 slashes for relative path
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize database with app
-# db.init_app(app)
+db.init_app(app)
 
 # Register routes
 register_routes(app)
 
 # Function to initialize database, change this to your own database initialization logic
-'''
 def init_db():
     with app.app_context():
         # Create all tables
         db.create_all()
         
         # Initialize database with data from init.json if empty
-        if Episode.query.count() == 0:
-            json_file_path = os.path.join(current_directory, 'init.json')
-            with open(json_file_path, 'r') as file:
+        if Film.query.count() == 0:
+            json_file_path = os.path.join(current_directory, 'data/final_movies.json')
+            with open(json_file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
-                for episode_data in data['episodes']:
-                    episode = Episode(
-                        id=episode_data['id'],
-                        title=episode_data['title'],
-                        descr=episode_data['descr']
+                for film_data in data:
+                    film = Film(
+                        tconst = film_data["tconst"],
+                        title = film_data["title"],
+                        year = int(film_data["year"]),
+                        genres = film_data["genres"],
+                        runtime = int(film_data["runtime"]),
+                        imdb_score = film_data["imdb_avg"],
+                        imdb_num_ratings = film_data["imdb_num_ratings"],
+                        tomatometer = film_data["tomatometer"],
+                        plot = film_data["plot"],
+                        rating = film_data["rating"],
+                        director = film_data["director"],
+                        actors = film_data["actors"],
+                        emotions = str(film_data["emotions"]),
+                        svd_embedding = str(film_data["svd_embedding"]),
+                        num_lb_reviews = film_data["num_lb_reviews"],
+                        num_imdb_reviews = film_data["num_imdb_reviews"],
+                        num_rt_reviews = film_data["num_rt_reviews"]
                     )
-                    db.session.add(episode)
-                
-                for review_data in data['reviews']:
-                    review = Review(
-                        id=review_data['id'],
-                        imdb_rating=review_data['imdb_rating']
-                    )
-                    db.session.add(review)
+                    db.session.add(film)
             
             db.session.commit()
-            print("Database initialized with episodes and reviews data")
-'''
-# init_db()
+            print("Database initialized with film data")
+init_db()
+
+# Extract posters
+print("Unzipping posters...")
+poster_dir = os.path.join(project_root, 'assets', 'posters')
+zip_path = os.path.join(project_root, 'assets', 'posters.zip')
+if not os.path.isdir(poster_dir):
+    with zipfile.ZipFile(zip_path, 'r') as zf:
+        for member in zf.infolist():
+            name = member.filename
+
+            if name.endswith('/'):
+                continue
+
+            if not name.startswith("posters/"):
+                continue
+
+            relative_path = name[len("posters/"):]
+            target_path = os.path.join(poster_dir, relative_path)
+
+            os.makedirs(os.path.dirname(target_path), exist_ok=True)
+
+            with zf.open(member) as source, open(target_path, "wb") as target:
+                target.write(source.read())
+print("Posters unzipped.")
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5001)
--- src/models.py
+++ src/models.py
@@ -2,22 +2,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Define Episode model
-class Episode(db.Model):
-    __tablename__ = 'episodes'
-    id = db.Column(db.Integer, primary_key=True)
+class Film(db.Model):
+    __tablename__ = 'films'
+    tconst = db.Column(db.String(64), primary_key=True)
     title = db.Column(db.String(64), nullable=False)
-    descr = db.Column(db.String(1024), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    genres = db.Column(db.String(1024))
+    runtime = db.Column(db.Integer, nullable=False)
+    imdb_score = db.Column(db.Float, nullable=False)
+    imdb_num_ratings = db.Column(db.Integer, nullable=False)
+    tomatometer = db.Column(db.Integer, nullable=True)
+    plot = db.Column(db.String(1024))
+    director = db.Column(db.String(128))
+    actors = db.Column(db.String(1024))
+    rating = db.Column(db.String(8))
+    emotions = db.Column(db.String(1024))
+    svd_embedding = db.Column(db.String(4096))
+    num_lb_reviews = db.Column(db.Integer)
+    num_imdb_reviews = db.Column(db.Integer)
+    num_rt_reviews = db.Column(db.Integer)
     
     def __repr__(self):
-        return f'Episode {self.id}: {self.title}'
-
-# Define Review model
-class Review(db.Model):
-    __tablename__ = 'reviews'
-    id = db.Column(db.Integer, primary_key=True)
-    imdb_rating = db.Column(db.Float, nullable=False)
-    
-    def __repr__(self):
-        return f'Review {self.id}: {self.imdb_rating}'
-
+        return f'Film {self.tconst}: {self.title}'
